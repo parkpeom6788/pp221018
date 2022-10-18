e0c2e4f37d3dc1382f26199c704be5fb19c67a17
parkpeom6788 <parkpeom6788@gmail.com>
2022-10-18 16:45:01 +0900

    인증 회원객체 직렬화 처리

--- webstudy88-pp1018/src/main/java/org/kosta/myproject/model/MemberVO.java
+++ webstudy88-pp1018/src/main/java/org/kosta/myproject/model/MemberVO.java
@@ -1,5 +1,9 @@
 package org.kosta.myproject.model;
 
-public class MemberVO {
+import java.io.Serializable;
 
+public class MemberVO implements Serializable {
+	// 리로딩 로그인풀리는걸 방지
+	private static final long serialVersionUID = -6079971115987091112L;
+	
 }
