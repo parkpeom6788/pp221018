430bb0279f3e0ec5ec9c02b42fb2c4336c2ba99a
parkpeom6788 <parkpeom6788@gmail.com>
2022-10-18 16:55:25 +0900

    iu member branch 에서 MemberVO id, register

--- webstudy88-pp1018/src/main/java/org/kosta/myproject/model/MemberVO.java
+++ webstudy88-pp1018/src/main/java/org/kosta/myproject/model/MemberVO.java
@@ -6,4 +6,12 @@ public class MemberVO implements Serializable {
 	// 리로딩 로그인풀리는걸 방지
 	private static final long serialVersionUID = -6079971115987091112L;
 	
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
 }
